270380259a06ee8cd664c032a3370a24f3dc7df0
hyunjin <gju08116@hanyang.ac.kr>
2024-01-30 00:50:21 +0900

    update memoizedPibo.js

--- js/exam-mine/memoizedPibo.js
+++ js/exam-mine/memoizedPibo.js
@@ -1,16 +1,16 @@
 //최종순수함수
 let finalrunCnt = 0;
 function memoized(fn) {
-	const memoizedTable = {};
-	return function (k) {
-		return memoizedTable[k] || (memoizedTable[k] = fn(k));
-	};
+  const memoizedTable = {};
+  return function (k) {
+    return memoizedTable[k] || (memoizedTable[k] = fn(k));
+  };
 }
 const memoizedPibo = memoized(function (n) {
-	finalrunCnt += 1;
-	if (n === 0) return 0;
-    if (n===1) return 1;
-	return memoizedPibo(n-1) + memoizedPibo(n - 2);
+  finalrunCnt += 1;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return memoizedPibo(n - 1) + memoizedPibo(n - 2);
 });
 const final_f5 = memoizedPibo(5);
 console.log("🚀 ~ final_f5:", final_f5, finalrunCnt);
