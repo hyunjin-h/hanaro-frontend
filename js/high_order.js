da81672bd1c05f0289af1b9cb1c016e2c816d192
hyunjin <gju08116@hanyang.ac.kr>
2024-01-30 17:40:47 +0900

    update high_order.js

--- js/high_order.js
+++ js/high_order.js
@@ -39,10 +39,37 @@ Array.prototype.mapX = function (f) {
 	}
 	return result;
 };
-const rets = arr.mapX(parseInt);
 
+const rets = arr.mapX(parseInt);
 //arr.map(function(item,idx,this))
 //parseInt('1',0,['1','2','3'])
 //parseInt('2',1,['1','2','3'])
 //parseInt('3',2,['1','2','3'])
+
 console.log(rets); //[1,NaN,NaN]
+
+const ret2_org = arr.mapX(function (item) {
+	return parseInt(item);
+});
+
+console.log("=====================");
+console.log("===unary-func-test===");
+const cb = (item) => parseInt(item);
+const ret2 = arr.mapX(cb);
+console.log("ret2:", ret2);
+
+const unary_org = function (fn) {
+	if (fn.length === 1) return fn;
+	return function (arg) {
+		return fn(arg);
+	};
+};
+
+const unary = (fn) => (fn.length === 1 ? fn : (arg) => fn(arg));
+const unaryCb = unary(cb);
+console.log("ret3: ", arr.mapX(unaryCb));
+
+const unaryParseInt = unary(parseInt);
+console.log("ret4: ", arr.mapX(unaryParseInt));
+
+console.log("ret5: ", arr.mapX(unary(parseInt)));
