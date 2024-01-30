49b53b3f120696f26439ef04db2ea116b8edc275
hyunjin <gju08116@hanyang.ac.kr>
2024-01-30 16:48:05 +0900

    update high_order.js

--- js/high_order.js
+++ js/high_order.js
@@ -10,6 +10,7 @@ function fx1(a) {
 }
 
 console.log("::>>", f1(fx1, 2));
+console.log("=====================");
 
 const f2_org = function (f) {
 	return function (...args) {
@@ -23,4 +24,25 @@ const f2 =
 
 const X = f2(Math.max);
 const result = X(1, 2, 3, 4, 5);
-console.log(result);
+console.log("result=>", result);
+console.log("=====================");
+
+const arr = new Array("1", "2", "3");
+
+//function parseInt(str)==> number
+constrets = arr.map(parseInt);
+
+Array.prototype.mapX = function (f) {
+	const result = [];
+	for (let i = 0; i < this.length; i += 1) {
+		result.push(f(this[i], i, this));
+	}
+	return result;
+};
+const rets = arr.mapX(parseInt);
+
+//arr.map(function(item,idx,this))
+//parseInt('1',0,['1','2','3'])
+//parseInt('2',1,['1','2','3'])
+//parseInt('3',2,['1','2','3'])
+console.log(rets); //[1,NaN,NaN]
