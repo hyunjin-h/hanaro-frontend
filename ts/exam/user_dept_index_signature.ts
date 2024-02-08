d5f447dcf035550fbbb04013fd8b1b04fc3164bc
hyunjin <gju08116@hanyang.ac.kr>
2024-02-08 17:26:44 +0900

    add ts file

--- ts/exam/user_dept_index_signature.ts
+++ ts/exam/user_dept_index_signature.ts
@@ -9,19 +9,14 @@ interface Dept {
 	captain: string;
 }
 
-// type Ud2 = (User | Dept) & { addr: string }; 이게 원래 정답
+// type Ud2 = (User | Dept) & {addr: string};
 
+// interface Ud2 extends Partial<User>, Partial<Dept> {
 interface Ud2 {
 	[idx: string]: string | number;
-	id: number; //id 안쓰면 감점이야 필수값이라서
-	// name?: string;
-	// dname?: string;
-	// captain?: string;
+	id: number;
 	addr: string;
 }
 
-// 다음 코드가 오류가 없으면 통과!
 const ud2: Ud2 = { id: 1, name: "HH", addr: "Seoul" };
 const ud3: Ud2 = { id: 1, dname: "HH", captain: "HH", addr: "Seoul" };
-
-export {};
--- ts/my-answer/array_tuple.ts
+++ ts/my-answer/array_tuple.ts
@@ -9,3 +9,4 @@ const sizeOption = { XS: 1, S: 5, M: 2, L: 2, XL: 4 };
 //고객이 주문한 상품의 총 금액을 요금표를 참고하여 계산하려고 한다. 에러가 발생하는 이유와 해결 방법을 고민해보자.
 const totalPrice: number = SIZE.reduce((currPrice, size) => currPrice + sizeOption[size.id] * size.price, 0);
 console.log(totalPrice);
+export {};
