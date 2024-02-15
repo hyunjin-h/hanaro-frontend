2ebb1c8966bdcae01f201659d07285cc11273233
hyunjin <gju08116@hanyang.ac.kr>
2024-02-15 10:25:05 +0900

    debounce, throttle

---
+++ ts/debounce.js
@@ -0,0 +1,13 @@
+const debounce = (cb, delay) => {
+	let timer;
+	return (...args) => {
+		if (timer) clearTimeout(timer);
+		timer = setTimeout(cb, delay, ...args);
+	};
+};
+const act = debounce((a) => a + 1, 1000);
+act(100);
+// 1초 동안 n번 호출 => 실행은 1회만!
+// 1초 후 => cb(100) 실행
+// 1.5초 후
+act(100);
